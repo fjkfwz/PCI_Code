cae39557cb0af4f88646128712a11042a9920cf1
MariCat <fjkfwz@hotmail.com>
2015-12-06 12:06:19 +0800

    Add Genetic Programming

--- Genetic Programming/__init__.py
+++ Genetic Programming/__init__.py
@@ -1,5 +1,20 @@
 import gp
 
 exampletree = gp.exampletree()
-exampletree.display()
-
+# exampletree.display()
+random1 = gp.makerandomtree(2)
+# print random1.evaluate([7, 1])
+# print random1.evaluate([2, 4])
+random2 = gp.makerandomtree(2)
+# print random2.evaluate([5, 3])
+# print random2.evaluate([5, 20])
+hiddenset = gp.buildhiddenest()
+# print gp.scorefunction(random2, hiddenset)
+# print gp.scorefunction(random1, hiddenset)
+# random2.display()
+muttree = gp.mutate(random2, 2)
+# muttree.display()
+# print gp.scorefunction(random2, hiddenset)
+# print gp.scorefunction(muttree, hiddenset)
+cross = gp.crossover(random1, random2)
+cross.display()
--- Genetic Programming/gp.py
+++ Genetic Programming/gp.py
@@ -20,10 +20,10 @@ class node:
         results = [n.evaluate(inp) for n in self.children]
         return self.function(results)
 
-    def display(self,indent = 0):
+    def display(self, indent=0):
         print (' ' * indent) + self.name
         for c in self.children:
-            c.display(indent+1)
+            c.display(indent + 1)
 
 
 class paramnode:
@@ -33,8 +33,8 @@ class paramnode:
     def evaluate(self, inp):
         return inp[self.idx]
 
-    def display(self,indent=0):
-        print '%sp%d' % (' '*indent,self.idx)
+    def display(self, indent=0):
+        print '%sp%d' % (' ' * indent, self.idx)
 
 
 class constnode:
@@ -44,8 +44,9 @@ class constnode:
     def evaluate(self, inp):
         return self.v
 
-    def display(self,indent=0):
-        print '%s%d' % (' '* indent,self.v)
+    def display(self, indent=0):
+        print '%s%d' % (' ' * indent, self.v)
+
 
 addw = fwrapper(lambda l: l[0] + l[1], 2, 'add')
 subw = fwrapper(lambda l: l[0] - l[1], 2, 'subtract')
@@ -80,3 +81,79 @@ def exampletree():
         node(subw, [paramnode(1), constnode(2)])
     ])
 
+
+def makerandomtree(pc, maxdepth=4, fpr=0.5, ppr=0.6):
+    if random() < fpr and maxdepth > 0:
+        f = choice(flist)
+        children = [makerandomtree(pc, maxdepth - 1, fpr, ppr)
+                    for i in range(f.childcount)]
+        return node(f, children)
+    elif random() < ppr:
+        return paramnode(randint(0, pc - 1))
+    else:
+        return constnode(randint(0, 10))
+
+
+def hiddenfunction(x, y):
+    return x ** 2 + 2 * y + 3 * x + 5
+
+
+def buildhiddenest():
+    rows = []
+    for i in range(200):
+        x = randint(0, 40)
+        y = randint(0, 40)
+        rows.append([x, y, hiddenfunction(x, y)])
+    return rows
+
+
+def scorefunction(tree, s):
+    dif = 0
+    for data in s:
+        v = tree.evaluate([data[0], data[1]])
+        dif += abs(v - data[2])
+    return dif
+
+
+def crossover(t1, t2, probswap=0.7, top=1):
+    if random() < probswap and not top:
+        return deepcopy(t2)
+    else:
+        result = deepcopy(t1)
+        if isinstance(t1, node) and isinstance(t2, node):
+            result.children = [crossover(c, choice(t2.children), probswap, 0) for c in t1.children]
+        return result
+
+
+def mutate(t, pc, probchange=0.1):
+    if random() < probchange:
+        return makerandomtree(pc)
+    else:
+        result = deepcopy(t)
+        if isinstance(t, node):
+            result.children = [mutate(c, pc, probchange) for c in t.children]
+        return result
+
+
+def evolve(pc, popsize, rankfunction, maxgen=500, mutationrate=0.1, breadinggrate=0.4, pexp=0.7, pnew=0.05):
+    def selectindex():
+        return int(log(random) / log(pexp))
+
+    population = [makerandomtree(pc) for i in range(popsize)]
+    for i in range(maxgen):
+        scores = rankfunction(population)
+        print scores[0][0]
+        if scores[0][0] == 0: break
+        newpop = [scores[0][1], scores[1][1]]
+        while len(newpop) < popsize:
+            if random() > pnew:
+                newpop.append(mutate(
+                    crossover(scores[selectindex()][1],
+                              scores[selectindex()][1],
+                              probswap=breadinggrate),
+                    pc, probchange=mutationrate))
+            else:
+                newpop.append(makerandomtree(pc))
+        population = newpop
+        scores[0][1].display()
+        return scores[0][1]
