c11a7ff61c525b096a7bf090232ab2d63a4cec2e
MariCat <fjkfwz@hotmail.com>
2015-12-06 22:56:01 +0800

    Add Genetic Programming

--- Genetic Programming/gp.py
+++ Genetic Programming/gp.py
@@ -135,9 +135,9 @@ def mutate(t, pc, probchange=0.1):
         return result
 
 
-def evolve(pc, popsize, rankfunction, maxgen=500, mutationrate=0.1, breadinggrate=0.4, pexp=0.7, pnew=0.05):
+def evolve(pc, popsize, rankfunction, maxgen=500, mutationrate=0.1, breedinggrate=0.4, pexp=0.7, pnew=0.05):
     def selectindex():
-        return int(log(random) / log(pexp))
+        return int(log(random()) / log(pexp))
 
     population = [makerandomtree(pc) for i in range(popsize)]
     for i in range(maxgen):
@@ -150,10 +150,88 @@ def evolve(pc, popsize, rankfunction, maxgen=500, mutationrate=0.1, breadinggrat
                 newpop.append(mutate(
                     crossover(scores[selectindex()][1],
                               scores[selectindex()][1],
-                              probswap=breadinggrate),
+                              probswap=breedinggrate),
                     pc, probchange=mutationrate))
-            else:
-                newpop.append(makerandomtree(pc))
+        else:
+            newpop.append(makerandomtree(pc))
         population = newpop
-        scores[0][1].display()
-        return scores[0][1]
+    scores[0][1].display()
+    return scores[0][1]
+
+
+def getrankfunction(dataset):
+    def rankfunction(population):
+        scores = [(scorefunction(t, dataset), t) for t in population]
+        scores.sort()
+        return scores
+
+    return rankfunction
+
+
+def gridgame(p):
+    max = (3, 3)
+    lastmove = [-1, -1]
+    location = [[randint(0, max[0]), randint(0, max[1])]]
+    location.append([(location[0][0] + 2) % 4, (location[0][1] + 2) % 4])
+    for o in range(50):
+        for i in range(2):
+            locs = location[i][:] + location[1 - i][:]
+            locs.append(lastmove[i])
+            move = p[i].evaluate(locs) % 4
+            if lastmove[i] == move: return 1 - i
+            lastmove[i] = move
+            if move == 0:
+                location[i][0] -= 1
+                if location[i][0] < 0: location[i][0] = 0
+            if move == 1:
+                location[i][0] += 1
+                if location[i][0] > max[0]: location[i][0] = max[0]
+            if move == 2:
+                location[i][1] -= 1
+                if location[i][1] < 0: location[i][1] = 0
+            if move == 3:
+                location[i][1] += 1
+                if location[i][1] > max[1]: location[i][1] = max[1]
+            if location[i] == location[1 - i]: return i
+    return -1
+
+
+def tournament(p1):
+    losses = [0 for p in p1]
+    for i in range(len(p1)):
+        for j in range(len(p1)):
+            if i == j: continue
+            winner = gridgame([p1[i], p1[j]])
+            if winner == 0:
+                losses[j] += 2
+            elif winner == 1:
+                losses[i] += 2
+            elif winner == -1:
+                losses[i] += 1
+                losses[i] += 1
+                pass
+        z = zip(losses, p1)
+        z.sort()
+        return z
+
+
+class humanplayer:
+    def evaluate(self, board):
+        me = tuple(board[0:2])
+        others = [tuple(board[x:x + 2]) for x in range(2, len(board) - 1, 2)]
+        for i in range(4):
+            for j in range(4):
+                if (i, j) == me:
+                    print 'O'
+                elif (i, j) in others:
+                    print 'X',
+                else:
+                    print '.',
+            print
+        print "YOU LAST MOVE WAS %d" % board[len(board) - 1]
+        print ' 0'
+        print '2 3'
+        print ' 1'
+        print 'Enter move:',
+        move = int(raw_input())
+        return move
